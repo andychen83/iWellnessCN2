215b800b03b7752cac2d2b19a5e4ff8918b4854d
＂andychen＂ <andylui83@163.com>
2017-01-06 00:12:27 +0800

    main page

---
+++ app/src/main/java/com/lefu/es/system/BodyFatNewActivity.java
@@ -0,0 +1,15 @@
+package com.lefu.es.system;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lefu.iwellness.newes.cn.system.R;
+
+public class BodyFatNewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_fat_new);
+    }
+}
