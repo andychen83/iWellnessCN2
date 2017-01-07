79a36b957796ebcee116e3a53598769060663ff5
JHoo1988 <339083113@qq.com>
2017-01-07 11:17:27 +0800

    人体秤

---
+++ app/src/main/java/com/lefu/es/system/BodyScaleNewActivity.java
@@ -0,0 +1,17 @@
+package com.lefu.es.system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lefu.iwellness.newes.cn.system.R;
+
+/*人体秤
+* */
+public class BodyScaleNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_scale_new);
+    }
+}
