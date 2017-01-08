fa8be6890dd0a154c6cace5b1cb36b5263c5d863
JHoo1988 <339083113@qq.com>
2017-01-08 15:55:12 +0800

    脂肪秤设置界面

--- app/src/main/java/com/lefu/es/system/BodyFatNewActivity.java
+++ app/src/main/java/com/lefu/es/system/BodyFatNewActivity.java
@@ -2,15 +2,20 @@ package com.lefu.es.system;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.lefu.iwellness.newes.cn.system.R;
 
-public class BodyFatNewActivity extends BaseBleActivity {
+public class BodyFatNewActivity extends BaseBleActivity implements View.OnClickListener {
 
+    private RelativeLayout set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_fat_new);
+        set = (RelativeLayout) findViewById(R.id.set);
+        set.setOnClickListener(this);
     }
 
     @Override
@@ -26,5 +31,16 @@ public class BodyFatNewActivity extends BaseBleActivity {
     @Override
     public void reveiveBleData(String data) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.set:
+                startActivity(BodyFatScaleSetActivity.creatIntent(BodyFatNewActivity.this));
+                break;
+            default:
+                break;
+        }
     }
 }
