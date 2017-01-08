77832ad1bf12a196effddb6dc4fd113f7221e87f
JHoo1988 <339083113@qq.com>
2017-01-08 14:35:59 +0800

    婴儿秤

---
+++ app/src/main/java/com/lefu/es/system/BabyScaleNewActivity.java
@@ -0,0 +1,17 @@
+package com.lefu.es.system;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lefu.iwellness.newes.cn.system.R;
+
+/*婴儿秤
+* */
+public class BabyScaleNewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_scale_new);
+    }
+}
