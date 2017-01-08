65b38e757ba798d191b518efb14200a725a53464
andychen83 <andylui83@163.com>
2017-01-08 11:31:40 +0800

    selector

--- app/src/main/java/com/lefu/es/system/BodyFatNewActivity.java
+++ app/src/main/java/com/lefu/es/system/BodyFatNewActivity.java
@@ -5,17 +5,20 @@ import android.os.Bundle;
 
 import com.lefu.iwellness.newes.cn.system.R;
 
+import butterknife.ButterKnife;
+
 public class BodyFatNewActivity extends BaseBleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_fat_new);
+        ButterKnife.bind(this);
     }
 
     @Override
     public void updateConnectionState(int resourceId) {
-        
+
     }
 
     @Override
@@ -26,5 +29,11 @@ public class BodyFatNewActivity extends BaseBleActivity {
     @Override
     public void reveiveBleData(String data) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
     }
 }
