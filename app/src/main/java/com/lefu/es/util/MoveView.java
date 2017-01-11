126e849c99f9d58857e7b7d281f9fef72cd01a24
JHoo1988 <339083113@qq.com>
2017-01-11 19:00:29 +0800

    等分

---
+++ app/src/main/java/com/lefu/es/util/MoveView.java
@@ -0,0 +1,16 @@
+package com.lefu.es.util;
+
+import android.view.View;
+
+/**
+ * 控制笑脸在水平上的偏移
+ * Created by lenovo on 2017/1/11.
+ */
+
+public class MoveView {
+
+    /*移动体重*/
+    public static void weight(View view, int sex) {
+
+    }
+}
