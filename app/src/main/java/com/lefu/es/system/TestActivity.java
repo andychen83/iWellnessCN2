0a7507e67b687e3e32a08025f135ac1f7792c658
JHoo1988 <339083113@qq.com>
2017-01-12 16:45:32 +0800

    偏移工具类

--- app/src/main/java/com/lefu/es/system/TestActivity.java
+++ app/src/main/java/com/lefu/es/system/TestActivity.java
@@ -4,19 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.lefu.es.util.MoveView;
 import com.lefu.iwellness.newes.cn.system.R;
 
 public class TestActivity extends AppCompatActivity {
     private ImageView face_img_weight;
     private LinearLayout face_img_weight_ll;
+    private TextView weight_critical_point1;
+    private TextView weight_critical_point2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-//        face_img_weight = (ImageView) findViewById(R.id.face_img_weight);
-//        face_img_weight.setPadding(50,0,0,0);
         face_img_weight_ll = (LinearLayout) findViewById(R.id.face_img_weight_ll);
-        face_img_weight_ll.setPadding(800,0,0,0);
+        weight_critical_point1 = (TextView) findViewById(R.id.weight_critical_point1);
+        weight_critical_point2 = (TextView) findViewById(R.id.weight_critical_point2);
+
+//        face_img_weight_ll.setPadding(800,0,0,0);
+        //(Activity activity, View view, TextView textView1, TextView textView2, int gender, int height, int weight)
+        MoveView.weight(TestActivity.this,face_img_weight_ll,weight_critical_point1,weight_critical_point2,0,170,56);
     }
 }
--- app/src/main/java/com/lefu/es/util/MoveView.java
+++ app/src/main/java/com/lefu/es/util/MoveView.java
@@ -1,6 +1,8 @@
 package com.lefu.es.util;
 
+import android.app.Activity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * 控制笑脸在水平上的偏移
@@ -9,8 +11,46 @@ import android.view.View;
 
 public class MoveView {
 
-    /*移动体重*/
-    public static void weight(View view, int split1,int split2,int weight) {
 
+    /**
+     * * 体重部分
+     *
+     * @param activity  当前Activity
+     * @param view      笑脸图标
+     * @param textView1 从左到右第一个分割线
+     * @param textView2 从左到右第二个分割线
+     * @param gender    性别：0男，1女
+     * @param height    身高
+     * @param weight    体重
+     */
+    public static void weight(Activity activity, View view, TextView textView1, TextView textView2, int gender, int height, int weight) {
+        // 标准体重
+        double standard_weight;
+        // 从左到右第一个分割线
+        double critical_point1;
+        // 从左到右第二个分割线
+        double critical_point2;
+        // 笑脸偏移量
+        int deviation = 0;
+        // 屏幕宽度
+        int screenwidth = activity.getWindowManager().getDefaultDisplay().getWidth();
+        if (gender == 0) {
+            standard_weight = (height - 80) * 0.7;
+        } else {
+            standard_weight = (height - 70) * 0.6;
+        }
+        critical_point1 = standard_weight - standard_weight * 0.1;
+        critical_point2 = standard_weight + standard_weight * 0.1;
+        if (weight < critical_point1) {
+            deviation = (int) (screenwidth * 0.25 * (weight / critical_point1));
+        } else if (weight >= critical_point1 && weight < critical_point2) {
+            deviation = (int) (screenwidth * 0.25 + (weight - critical_point1) / (critical_point2 - critical_point1) * screenwidth * 0.5);
+        } else if (weight >= critical_point2) {
+            deviation = (int) (screenwidth * 0.75 + (weight - critical_point2) / standard_weight * 0.1 * screenwidth * 0.25);
+        }
+        textView1.setText(String.valueOf(MyUtil.onePoint(critical_point1)));
+        textView2.setText(String.valueOf(MyUtil.onePoint(critical_point2)));
+
+        view.setPadding(deviation-10, 0, 0, 0);
     }
 }
